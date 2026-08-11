4b4fc71b58c3ac361f20de99026a5a0537d4b6d7
harisharumugam94 <priyaharish055@gmail.com>
2026-08-11 21:52:56 +0530

    complete the ecommmerce site

---
+++ admin-app/src/api/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// Change this to your live Render URL once the backend is deployed
+export const API_BASE = 'http://localhost:5001/api';
+
+// URL of the separate customer-facing User app
+export const USER_APP_URL = 'http://localhost:5173';
+
+const api = axios.create({ baseURL: API_BASE });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('adminToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
